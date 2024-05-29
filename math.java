dd53863f640577dcc250e3f26e5499715eae71d0
Henrique Alves <162392311+HenriqueAlper@users.noreply.github.com>
2024-05-29 09:41:34 -0300

    Add files via upload

--- math.java
+++ math.java
@@ -9,10 +9,15 @@ class Main {
         double valueThree = Math.floor(7.9);
         // Math.random - Gera números aleatórios de 0.0 até 1.0, geralmente usado para sorteios
         // Math.random() * (numero qualquer) - Será gerado números de 0.0 até o numero qualquer que você definir
-        double valueFour = Math.random();
+        double valueFour = Math.random()*100;
+        // É possivel incluir classes dentro de outras classes, por exemplo, caso queira que um random saia arredondado é so incluir na classe Math.round
+        // caso queira fazer uma integer e de errado mesmo com as classes em double é so utilizar o "(int)" no começo do stetaments 
+        int valorFive = (int)Math.round(Math.random()*200); 
         System.out.println(value);
         System.out.println(valueTwo);
-        System.out.println(valueThree);  
+        System.out.println(valueThree); 
+        System.out.println(valueFour); 
+        System.out.println(valorFive);
     }
     
 }
